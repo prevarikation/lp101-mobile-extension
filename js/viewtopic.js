bb9cff29fa22fe7933da4a6b577230382a68f752
prevarikation <81432187+prevarikation@users.noreply.github.com>
2023-06-22 09:36:35 -0600

    Add extra "Return to forum" link at top of thread.

--- js/viewtopic.js
+++ js/viewtopic.js
@@ -1,2 +1,11 @@
+// add extra "Return to forum" navigation at top of thread
+const returnToForumEl = document.querySelector('[accesskey=r]').cloneNode(true);
+returnToForumEl.removeAttribute('accesskey');
+returnToForumEl.classList.remove('left-box');
+const forumReturnContainingPara = document.createElement('p');
+forumReturnContainingPara.style = 'margin: 0.5em 0 0 0';
+forumReturnContainingPara.appendChild(returnToForumEl);
+document.getElementById('content').prepend(forumReturnContainingPara);
+
 // imgur embeds break smooth shrinking because they have inline styles of style="max-width: 800px;max-height: 800px"
 document.querySelectorAll(".postbody .content img").forEach(el => el.style.removeProperty('max-width'));
